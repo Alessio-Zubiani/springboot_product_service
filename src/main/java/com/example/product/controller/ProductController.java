8e77555570eba7e32f09d814c67622514f16b217
4631 <alessio.zubiani@popso.it>
2024-11-04 16:50:56 +0100

    Aggiunto CircuitBreaker

--- src/main/java/com/example/product/controller/ProductController.java
+++ src/main/java/com/example/product/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.example.product.dto.ProductResponse;
 import com.example.product.service.ProductService;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:9000")
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
